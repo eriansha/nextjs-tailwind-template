810ebb2c12a650f15b6b1a0d8e5481231f6536cd
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-02-04 15:37:01 +0700

    chore(eslint): apply eslint-config-prettier
    apply eslint rule to disable particular rule that might conflict with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,16 @@ module.exports = {
     "eslint:recommended",
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   plugins: ["@typescript-eslint"],
   root: true,
   rules: {
     "no-unused-vars": "off",
     "no-console": "warn",
-    "quotes": ["error", "double"],
   },
   globals: {
     React: true,
     JSX: true,
   },
-};
+}
