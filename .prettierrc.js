4930ac92b376b9c6ff7117abd35cbabc363c4326
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-02-04 15:36:04 +0700

    chore(prettier): init prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * List of applied prettier rule. See the detail of those rules below
+ * @link https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+}
